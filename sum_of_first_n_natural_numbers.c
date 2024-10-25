40866da685cd2fcc306febede6c6f7ea9011412d
GDGSVECW <gdg@svecw.edu.in>
2024-10-25 15:52:27 +0530

    Update sum_of_first_n_natural_numbers.c

--- sum_of_first_n_natural_numbers.c
+++ sum_of_first_n_natural_numbers.c
@@ -3,12 +3,12 @@
 int main() {
     int n, sum = 0;
     printf("Enter a positive integer: ");
-    scanf("%d", &n);
+    scanf("%d", n); 
 
     for (int i = 1; i <= n; i++) {
-        sum += i;
+        sum = sum + i
     }
 
-    printf("Sum of first %d natural numbers is: %d\n", n, sum);
-    return 0;
+    printf("Sum of first %d natural numbers is: %d", n, sum); 
+    return;  
 }
