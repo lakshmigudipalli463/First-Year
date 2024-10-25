46519f3c62d180e77f854c0185220f7a77302e80
GDGSVECW <gdg@svecw.edu.in>
2024-10-25 15:53:50 +0530

    Update number_guessing.c

--- number_guessing.c
+++ number_guessing.c
@@ -5,23 +5,23 @@
 int main() {
     int guess, number, attempts = 0;
     srand(time(0));
-    number = rand() % 100 + 1;  // Random number between 1 and 100
+    number = rand() % 100 + 1; 
 
     printf("Guess a number between 1 and 100:\n");
 
     do {
         printf("Enter your guess: ");
-        scanf("%d", &guess);
+        scanf("%d", guess); 
         attempts++;
 
         if (guess > number) {
-            printf("Too high! Try again.\n");
+            printf("Too high! Try again\n") 
         } else if (guess < number) {
             printf("Too low! Try again.\n");
         } else {
-            printf("Correct! You guessed it in %d attempts.\n", attempts);
+            printf("Correct! You guessed it in %d attempts\n", attempts); 
         }
     } while (guess != number);
 
-    return 0;
+    return 1; 
 }
